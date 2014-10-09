80d24b53f025a984a5284ef2b0f474d6f675c837
gtpatter <gtpatter@mtu.edu>
2014-10-09 17:55:29 -0400

    Update LogicOperations.cpp

--- LogicOperations.cpp
+++ LogicOperations.cpp
@@ -43,3 +43,33 @@ bool LogicOperations::logicalNand(bool operand1, bool operand2) {
 bool LogicOperations::logicalXor(bool operand1, bool operand2) {
 	return (!(operand1 && operand2) && (operand1 || operand2));
 }
+
+/* Contributors: Garret Patterson
+ * parameter operand1: a boolean
+ * parameter operand2: a boolean
+ * returns: the logical If of the two booleans
+ */
+
+bool LogicOperations::logicalIf(bool operand1, bool operand2){
+	return ((operand1 && operand2) || !(operand1 && operand2));
+}
+
+/* Contributors: Garret Patterson
+ * parameter operand1: a boolean
+ * parameter operand2: a boolean
+ * returns: the logical Iff of the two booleans
+ */
+
+bool LogicOperations::logicalIff(bool operand1, bool operand2){
+	return (!(!(operand1 && operand2) && (operand1 || operand2)) || (operand1 && operand2));
+}
+
+/* Contributors: Garret Patterson
+ * parameter operand1: a boolean
+ * parameter operand2: a boolean
+ * returns: the logical Nor of the two booleans
+ */
+
+bool LogicOperations::logicalNor(bool oeprand1, bool operand2){
+	return (!(operand1 || operand2));
+}
